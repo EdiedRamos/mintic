5f441960b6d191ac60d56f59594a68c36584fa4e
EdiedRamos <edieisa22@gmail.com>
2021-07-28 20:48:11 -0500

    primeros pasos en jdbc

---
+++ ciclo2/clases/unidad4/java-JDBC/Java_JDBC/src/main/java/proyecto/borrar/java_jdbc/Main.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.borrar.java_jdbc;
+
+/**
+ *
+ * @author ediei
+ */
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        try {
+            
+            // crear la conexión
+            
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/aprendiendo", "root", "");
+            
+            // crear objeto statement
+            
+            Statement sentencia = conexion.createStatement();
+            
+            // Ejercutar sql
+            
+            ResultSet respuesta = sentencia.executeQuery("SELECT DISTINCT POBLACIÓN FROM CLIENTES");
+            
+            while (respuesta.next()) {
+                System.out.println("Empresa -> " + respuesta.getString("POBLACIÓN"));
+            }
+            
+        } catch(Exception e) {
+            System.err.println(e);
+        }
+        
+    }
+}
---
+++ ciclo2/retos/reto2/README.md
@@ -0,0 +1,3 @@
+## Reto2
+
+Solución al reto 2 de mintic
